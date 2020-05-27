1850984e295a316a3e0560a385d116f9284a8f36
3dzbot <zoduaks@ukr.net>
2020-05-27 12:11:24 +0300

    less1

---
+++ script/main.js
@@ -0,0 +1,24 @@
+const leftMenu = document.querySelector('.left-menu');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', ()=>{
+  leftMenu.classList.toggle('openMenu');
+  hamburger.classList.toggle('open');
+});
+
+document.addEventListener('click', (e)=>{
+  if(!e.target.closest('.left-menu')){
+    leftMenu.classList.remove('openMenu');
+    hamburger.classList.remove('open');
+  }
+});
+
+leftMenu.addEventListener('click', (e)=>{
+  let target = e.target;
+  const dropdown = target.closest('.dropdown');
+  if(dropdown){
+    dropdown.classList.toggle('active');
+    leftMenu.classList.add('openMenu');
+    hamburger.classList.add('open');
+  }
+});
